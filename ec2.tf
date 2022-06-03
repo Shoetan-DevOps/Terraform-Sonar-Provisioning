da94f704bfe088ee0afc63a1845978cae48777b8
alfred <alfredshoetan@gmail.com>
2022-06-03 05:02:10 -0400

    added sonar install script

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "sonarqube" {
     ami = var.my-ami-list["RHEL"]
     instance_type = var.my-instance-type[1]
-    #user_data = file("${path.module}/install-sonar.sh")
+    user_data = file("${path.module}/install-sonar.sh")
     key_name = var.my-key-pem
 
     vpc_security_group_ids = [aws_security_group.allow-sonar-ssh.id, aws_security_group.allow-sonar-web.id]
---
+++ install-sonar.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+# Script to install Jenkins 
+<<ml
+     Hige level Steps
+     1) create Sonar User & give admin rights
+     2) install depedndencies - wget, java jdk, unzip
+     3) download SonarQube
+     4) change ownership & group to Sonar and set permission to 775 
+     5) start sonar => sonar.sh start
+ml
+
+
+# Step 0: Set Hostname
+sudo hostnamectl set-hostname Jenkins
+
+# Step 1: Create Sonar User
+sudo useradd sonar
+sudo echo "sonar ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/sonar
+su - sonar
+
+# Step 2: cd to /opt install dependencies wget, unzip
+cd /opt
+sudo yum install wget unzip -y
+
+# Step 3: Install Java jdk
+sudo wget sudo wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm
+sudo yum install jdk-8u131-linux-x64.rpm -y
+
+# Step 4: Download Sonar & unzip & Rename
+sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-7.8.zip
+sudo unzip sonarqube-7.8.zip
+sudo rm -rf sonarqube-7.8.zip
+sudo mv sonarqube-7.8/ sonarqube/
+
+# Step 5: Set ownership, group & permission for SonarQube
+sudo chown -R sonar:sonar /opt/sonarqube
+sudo chmod -R 775 /opt/sonarqube
+
+# Step 6: Start SonarQube
+sh /opt/sonarqube/bin/linux*/sonar.sh start
